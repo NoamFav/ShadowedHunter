e062ad9caec8c3f6afae4fa1dd8db3681d078d78
noamfav <95364866+NoamFav@users.noreply.github.com>
2024-12-29 15:46:52 +0100

    modified src

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module com.ShadowedHunter {
     // External dependencies
     requires jlayer; // For MP3 playback
     requires org.slf4j; // For logging API
-    requires ch.qos.logback.classic; // Logback implementation (if used)
+    requires ch.qos.logback.classic;
     requires ch.qos.logback.core;
 
     // Exporting your package
